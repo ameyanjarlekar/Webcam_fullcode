a8aca6f7cbfbfadb70174dd5fc5f6d1f6ce9df39
ameyanjarlekar <ameyanjarlekar@gmail.com>
2018-12-25 12:01:39 +0530

    initial files

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,10 @@
+# CMake generated Testfile for 
+# Source directory: /home/ameya/balldetec_ws/src
+# Build directory: /home/ameya/balldetec_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("ip_cam_10")
+subdirs("usb_cam")
+subdirs("video_stream_opencv")
---
+++ build/ip_cam_10/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/ameya/balldetec_ws/src/ip_cam_10
+# Build directory: /home/ameya/balldetec_ws/build/ip_cam_10
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
